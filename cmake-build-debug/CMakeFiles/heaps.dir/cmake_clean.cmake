66ce6801d62d07092a1c5cd55876e4a3d1b58997
Mikhail Lobanov <mp.lobanov@yandex.ru>
2019-12-12 13:00:58 +0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/heaps.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/heaps.dir/main.cpp.o"
+  "heaps"
+  "heaps.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/heaps.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
